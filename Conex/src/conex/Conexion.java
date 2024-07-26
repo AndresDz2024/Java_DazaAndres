2911ad100326d967d1ae8bef9bbb0ea21d020d5a
AndresDz2024 <daza45daza@gmail.com>
2024-07-26 00:36:54 -0500

    feat: :sparkles: CRUD de hospitales
    Se creó satisfactoriamente el crud para la tabla hospitales conectada desde mysql

--- Conex/src/conex/Conexion.java
+++ Conex/src/conex/Conexion.java
@@ -28,7 +28,7 @@ public class Conexion {
                 "umebuygci6oisixr",
                 "hW7IZzjTQX2ahZG8x9zT"
             );
-            JOptionPane.showMessageDialog(null, "La conexión es un éxito :)");
+            //JOptionPane.showMessageDialog(null, "La conexión es un éxito :)");
         } catch (Exception e) {
             System.err.println("Error en la conexión :(, error:" + e);
             JOptionPane.showMessageDialog(null, "Error en la conexión: " + e.toString());
@@ -53,6 +53,84 @@ public class Conexion {
             System.err.println("Error al ejecutar la consulta: " + e);
         }
     }
+    
+    public void InsertHospitales(){
+        Conexion cn = new Conexion();
+        Scanner sc = new Scanner(System.in);
+        PreparedStatement ps = null;
+        Connection con = cn.establecerConexion();
+        String nombreHospital;
+        String direccionHospital;
+        try {
+            ps = con.prepareStatement("insert into hospitales (nombre, direccion) values (?,?)");
+            System.out.println("Ingresa el nombre del hospital: ");
+            nombreHospital = sc.nextLine();
+            System.out.println("Ingresa la direccion del hospital: ");
+            direccionHospital = sc.nextLine();
+            ps.setString(1, nombreHospital);
+            ps.setString(2, direccionHospital);
+            ps.executeUpdate();
+            System.out.println("Insercion realizada con éxito");
+        } catch (Exception e){
+            System.err.println("Error sl ejecutar la inserción: " + e);
+        }
+    }
+    
+    public void DeleteHospitales(){
+        Conexion cn = new Conexion();
+        Scanner sc = new Scanner(System.in);
+        PreparedStatement ps = null;
+        Connection con = cn.establecerConexion();
+        int idHospital;
+        try {
+            ps = con.prepareStatement("delete from hospitales where id_hospital = ?");
+            System.out.println("Ingresa el id del hospital a eliminar: ");
+            idHospital = sc.nextInt();
+            ps.setInt(1, idHospital);
+            ps.executeUpdate();
+            System.out.println("Hospital eliminado con éxito");
+        } catch(Exception e){
+            System.err.println("Error sl eliminar los datos: " + e);            
+        }
+
+    }
+    
+    public void UpdateHospitales() {
+        Conexion cn = new Conexion();
+        Scanner sc = new Scanner(System.in);
+        PreparedStatement ps = null;
+        Connection con = cn.establecerConexion();
+        String nombreHospital;
+        String direccionHospital;
+        int idHospital;
+    
+        try {
+            System.out.println("Ingresa el id del hospital que deseas actualizar: ");
+            idHospital = sc.nextInt();
+            sc.nextLine(); 
+
+            System.out.println("Ingresa el nuevo nombre del hospital: ");
+            nombreHospital = sc.nextLine();
+        
+            System.out.println("Ingresa la nueva direccion del hospital: ");
+            direccionHospital = sc.nextLine();
+        
+            ps = con.prepareStatement("UPDATE hospitales SET nombre = ?, direccion = ? WHERE id_hospital = ?");
+            ps.setInt(3, idHospital);
+            ps.setString(1, nombreHospital);
+            ps.setString(2, direccionHospital);
+        
+            int rowsUpdated = ps.executeUpdate();
+            if (rowsUpdated > 0) {
+                System.out.println("La actualización fue exitosa.");
+            } else {
+                System.out.println("No se encontró un hospital con el id proporcionado.");
+            }   
+        } catch (Exception e) {
+            System.err.println("Error al actualizar los datos: " + e);
+        }     
+    }
+    
     public static void main(String[] args) {
         Conexion cn = new Conexion();
         Scanner sc = new Scanner(System.in);
@@ -62,13 +140,23 @@ public class Conexion {
         ResultSet rs;
         int option;
         do {
-            System.out.printf("Bienvenido Usuario%n ¿Que deseas hacer el dia de hoy? ");
+            System.out.printf("Bienvenido Usuario%n¿Que deseas hacer el dia de hoy?%n");
+            System.out.printf("1.Ver Hospitales Registrados%n2. Registrar Nuevo Hospital%n3. Actualizar Hospitales Registrados%n4. Eliminar Hospitales Registrados%n5. Salir%n");
             option = sc.nextInt();
             switch (option){
                 case 1:
                     cn.VerHospitales();
                     break;
+                case 2:
+                    cn.InsertHospitales();
+                    break;
+                case 3:
+                    cn.UpdateHospitales();
+                    break;
+                case 4:
+                    cn.DeleteHospitales();
             }
         } while (option != 5);
+        System.out.println("Hasta pronto Usuario");
     }
 }
